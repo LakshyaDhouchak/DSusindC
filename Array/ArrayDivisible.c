a6317970136eeda68c2add0a8e62a556271c4e23
lakshya <lakshya10171@gmail.com>
2024-08-28 18:39:02 +0530

    arrayDivisible commit

---
+++ Array/ArrayDivisible.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int size;
+    int i;
+    int sum=0;
+    printf("Enter the size of an array:");
+    scanf("%d",&size);
+    int arr[size];
+    for(i=0;i<size;i++){
+        printf("Enter the data:");
+        scanf("%d",& arr[i]);
+    }
+    for(i=0;i<size;i++){
+        if(arr[i]%3 ==0){
+            sum= sum+arr[i];
+        }
+        
+    }
+    printf("%d",sum);
+    return 0;
+}
